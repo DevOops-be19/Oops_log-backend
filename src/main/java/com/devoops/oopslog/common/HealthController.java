1755cf172a0aeae6b76220c225b18d480f0393f1
Kjandgo <rptmffld0204@gmail.com>
2025-11-13 09:21:37 +0900

    no message

---
+++ src/main/java/com/devoops/oopslog/common/HealthController.java
@@ -0,0 +1,12 @@
+package com.devoops.oopslog.common;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HealthController {
+    @GetMapping("health")
+    public String health() {
+        return "I'm Oops!";
+    }
+}
